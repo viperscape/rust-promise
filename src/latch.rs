17ec100155087cda516500a28d359cc102d6d96a
Chris Gill <chrisfgill@gmail.com>
2015-03-02 09:11:43 -0500

    added reopen latch

--- src/latch.rs
+++ src/latch.rs
@@ -10,12 +10,24 @@ impl Latch {
     pub fn new () -> Latch {
         Latch { latch: Arc::new(AtomicBool::new(false)) }
     }
+
+    /// close latch
     pub fn close (&self) -> bool {
         if !self.latch.compare_and_swap(false,true,Ordering::Release) {true}
         else {false}
     }
+
+    /// is latch closed?
     pub fn latched (&self) -> bool {
         self.latch.load(Ordering::Acquire)
+    }
+
+    /// reopens latch, if necessary; do not use for stateful checks
+    pub fn open (&self) -> bool {
+        if self.latched() {
+            self.latch.compare_and_swap(true,false,Ordering::Release)
+        }
+        else { true }
     }
 }
 
@@ -32,6 +44,8 @@ mod tests {
         assert_eq!(l.close(),true);
         assert_eq!(l.close(),false); //subsequent latching fails, already latched
         assert_eq!(l.latched(),true);
+        assert_eq!(l.open(),true);
+        assert_eq!(l.latched(),false);
     }
 
     #[bench]
